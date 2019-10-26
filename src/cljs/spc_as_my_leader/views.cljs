9eeaa6f8323e8886c9583bad5fc2af2fa99a5955
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-10-27 00:11:12 +0200

    Create initial slide with public domain svgs

--- src/cljs/spc_as_my_leader/views.cljs
+++ src/cljs/spc_as_my_leader/views.cljs
@@ -30,36 +30,47 @@
       (getElementById "main-panel")
       requestFullscreen))
 
+(defn title-slide []
+  [:div.card-content
+   [:div.columns>div.column>div.hero.is-primary>div.hero-body>div.container
+    [:h1.title.is-1 "SPC as my leader"]
+    [:h2.subtitle.is-3 "programming Clojure with Spacemacs"]]
+   [:div.columns.is-mobile
+    [:div.column.is-4.is-offset-2>figure.image.is-256x256>img
+     {:src "/img/clj-logo.svg"}]
+    [:div.column.is-4>figure.image.is-256x256>img
+     {:src "/img/spacemacs-logo.svg"}]]
+   [:div.columns>div.column>i.is-size-5 "Erwin Rooijakkers"]])
+
 (defn intro-slide []
-  [:div.card
-   [:div.card-header.card-header-title "INTRO"]
-   [:div.card-content
-    [:p "My experience. Cannot do deep dive. Just glimpse."]
-    [:p "Why clojure? data, fp, lisp, but also this editor. cannot work if i cannot use spcmacs "]
-    [:p "Why Vim?"]
-    [:p
-     [:pre
-      (string/join
-       "\n"
-       ["vi is [[13~^[[15~^[[15~^[[19~^[[18~^ a "
-        "muk[^[[29~^[[34~^[[26~^[[32~^ch better editor than this emacs. I know "
-        "I^[[14~'ll get flamed for this but the truth has to be "
-        "said. ^[[D^[[D^[[D^[[D ^[[D^[^[[D^[[D^[[B^ "
-        "exit ^X^C quit :x :wq dang it :w:w:w :x ^C^C^Z^D"])]
-     [:i "— Jesper Lauridsen from alt.religion.emacs"]]
-    [:p "ci .)"]
-    [:p "SPC"]
-    [:p "rrrrr"]
-    [:p "Evil as my leader"]
-    [:div.content
-     [:li "Spacemacs"
-      [:ul
-       [:li "batteries included"]
-       [:li "layers - ,"]
-       [:li "logical bindings, self discoverable"]]]
-     [:li "Clojure(Script)"
-      [:ul
-       [:li "Lisp on the JVM and JavaScript runtime, immutable data structures"]]]]]])
+  [:div.card-content
+   [:h1.title "INTRO"]
+   [:p "My experience. Cannot do deep dive. Just glimpse."]
+   [:p "Why clojure? data, fp, lisp, but also this editor. cannot work if i cannot use spcmacs "]
+   [:p "Why Vim?"]
+   [:p
+    [:pre
+     (string/join
+      "\n"
+      ["vi is [[13~^[[15~^[[15~^[[19~^[[18~^ a "
+       "muk[^[[29~^[[34~^[[26~^[[32~^ch better editor than this emacs. I know "
+       "I^[[14~'ll get flamed for this but the truth has to be "
+       "said. ^[[D^[[D^[[D^[[D ^[[D^[^[[D^[[D^[[B^ "
+       "exit ^X^C quit :x :wq dang it :w:w:w :x ^C^C^Z^D"])]
+    [:i "— Jesper Lauridsen from alt.religion.emacs"]]
+   [:p "ci .)"]
+   [:p "SPC"]
+   [:p "rrrrr"]
+   [:p "Evil as my leader"]
+   [:div.content
+    [:li "Spacemacs"
+     [:ul
+      [:li "batteries included"]
+      [:li "layers - ,"]
+      [:li "logical bindings, self discoverable"]]]
+    [:li "Clojure(Script)"
+     [:ul
+      [:li "Lisp on the JVM and JavaScript runtime, immutable data structures"]]]]])
 
 (defn evil-slide []
   [:li "EVIL mode"
@@ -109,27 +120,27 @@
     [:li "start using it at work"]]])
 
 (def slide-deck
-  [[intro-slide]
-   [evil-slide]
+  [[title-slide]
+   [intro-slide]
    [cider-slide]
+   [evil-slide]
    [structural-editing-slide]
    [clj-refactor-slide]
    [conclusion-slide]])
 
 ;; v = f(s), view v is function f of application state s
 (defn view [state]
-  [:div#main-panel
-   [:section.section
-    [:ol "zaken" (:page state)
-     (get slide-deck (:page state))]]
+  [:section
+   [:section#main-panel.section>div.card
+    (get slide-deck (:page state))]
    [:footer.footer
-    [:nav.pagination.is-large
+    [:nav.pagination
      [:button.button.pagination-previous
       {:on-click previous-page!} "previous"]
      [:button.button.pagination-next
-      {:on-click next-page!} "next"]]
-    [:button.button {:on-click request-full-screen}
-     "full screen"]]])
+      {:on-click next-page!} "next"]
+     [:button.button {:on-click request-full-screen}
+      "full screen"]]]])
 
 (defn main-panel []
   [view @app-state])
