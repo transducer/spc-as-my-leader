7cc193d483d9d6df1988102bbc6ab6dc76f63887
Erwin Rooijakkers <erwin@rooijakkers.software>
2019-10-27 14:49:49 +0100

    Debugging and macroexpansion

--- src/clj/build.clj
+++ src/clj/build.clj
@@ -1,8 +1,18 @@
 (ns build)
 
-(defn map* [f coll]
+(defn map-2 [f coll]
   (when (seq coll)
     (cons (f (first coll))
           (map* f (rest coll)))))
 
-(map* inc [1 2])
+(map-2 inc [1 2 3])
+
+
+
+
+
+
+(defmacro unless [pred & clauses]
+  `(if (not ~pred) ~@clauses))
+
+(unless (= 2 1) (println "evaluated"))
